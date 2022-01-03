72a9dab7433190d04adaf921681409618eb74a4b
fbattello <fabien.battello@orange.com>
2022-01-03 15:36:04 +0100

    Added scheduler w/ background thread

--- pyngsild/src/pyngsild/agent/scheduler.py
+++ pyngsild/src/pyngsild/agent/scheduler.py
@@ -13,7 +13,7 @@
 import anyio
 import time
 import schedule
-import _thread
+import threading
 import logging
 
 from datetime import datetime
@@ -21,12 +21,14 @@ from typing import Callable
 from dataclasses import dataclass
 from enum import Enum
 
-from .daemon import ManagedDaemon
+from .daemon import ManagedDaemon, State
 from .agent import Agent
 
 logger = logging.getLogger(__name__)
 
 # TODO : run the whole thing backgroung using a thread !!
+# TODO : add a ThreadPoolExecutor to deal with NGSI API requests
+
 
 class UNIT(Enum):
     seconds = "s"
@@ -36,19 +38,38 @@ class UNIT(Enum):
 
 
 class Scheduler(ManagedDaemon):
-    """Poll takes an agent and polls at periodic intervals.
-    """
+    """Poll takes an agent and polls at periodic intervals."""
 
-    def __init__(self, agent: Agent, func: Callable = None, interval: int = 1, unit: UNIT = UNIT.minutes):
+    def __init__(
+        self,
+        agent: Agent,
+        func: Callable = None,
+        interval: int = 1,
+        unit: UNIT = UNIT.minutes,
+    ):
         # take an already created agent and reschedule it
         # 1 - a func may be provided to create a new Source
-        # 2 - or the source may have a reset() method        
+        # 2 - or the source may have a reset() method
         # 3 - or the Source __init__() is called to reinit the Source
         super().__init__(agent.sink, agent.process)
         self.src = agent.src
         self.func = func
         self.interval = interval
         self.unit = unit
+
+        logger.info("schedule job")
+        if self.unit == UNIT.seconds:
+            schedule.every(self.interval).seconds.do(self.job)
+            self.tick = 1
+        elif self.unit == UNIT.minutes:
+            schedule.every(self.interval).minutes.do(self.job)
+            self.tick = 4
+        elif self.unit == UNIT.hours:
+            schedule.every(self.interval).hours.do(self.job)
+            self.tick = 32
+        elif self.unit == UNIT.days:
+            schedule.every(self.interval).days.do(self.job)
+            self.tick = 128
 
     async def _ajob(self):
         logger.info(f"start new job at {datetime.now()}")
@@ -57,37 +78,34 @@ class Scheduler(ManagedDaemon):
 
         # "reinit" the Source
         if self.func is not None:
-            self.src = self.func() # get a new Source if such a method has been provided
+            self.src = (
+                self.func()
+            )  # get a new Source if such a method has been provided
         else:
-            src = self.src                
+            src = self.src
             if hasattr(src, "reset"):
                 src.reset()
             else:
                 src.__init__()
 
-        await self.trigger(src)    
+        await self.trigger(src)
 
     def job(self):
         anyio.run(self._ajob)
 
+    def _loop(self):
+        while self.status.state != State.CLOSED:
+            logger.debug("tick")
+            schedule.run_pending()
+            time.sleep(self.tick)
+
     def run(self):
         super().run()
         self.job()
-        logger.info("schedule job")
-        if self.unit == UNIT.seconds:
-            schedule.every(self.interval).seconds.do(self.job)
-            tick = 1
-        elif self.unit == UNIT.minutes:
-            schedule.every(self.interval).minutes.do(self.job)
-            tick = 4
-        elif self.unit == UNIT.hours:
-            schedule.every(self.interval).hours.do(self.job)
-            tick = 32
-        elif self.unit == UNIT.days:
-            schedule.every(self.interval).days.do(self.job)
-            tick = 128
 
-        while True:
-            logger.debug("tick")
-            schedule.run_pending()
-            time.sleep(tick)
+        thread = threading.Thread(target=self._loop)
+        thread.start()
+
+    def close(self):
+        schedule.cancel_job(self.job)
+        super().close()
